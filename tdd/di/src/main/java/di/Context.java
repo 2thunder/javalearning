79438d9b4dbf646dbe9fa3a14d0e300dae7a9473
yutianlei <yutianlei@shein.com>
2022-05-13 13:28:06 +0800

    refactor: 绑定逻辑

--- tdd/di/src/main/java/di/Context.java
+++ tdd/di/src/main/java/di/Context.java
@@ -1,13 +1,16 @@
 package di;
 
+import com.sun.jdi.InvocationException;
 import jakarta.inject.Inject;
 import jakarta.inject.Provider;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Context {
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
@@ -17,47 +20,34 @@ public class Context {
     }
 
     public <Type, Implementation> void bind(Class<Type> componentClass, Class<Implementation> implementation) {
-        Constructor<?>[] injectedConstructors = Arrays.stream(implementation.getConstructors()).filter(c -> c.isAnnotationPresent(Inject.class))
-            .toArray(Constructor<?>[]::new);
-
-        if(injectedConstructors.length > 1){
-            throw new IllegalComponentException();
-        }
-
-        // 如果没有注入构造函数，则取默认构造函数, 如果默认构造函数没有，则抛出异常
-        if(injectedConstructors.length == 0 && Arrays.stream(implementation.getConstructors()).filter(c -> c.getParameters().length == 0).findFirst().map(c -> false).orElse(true)){
-            throw new IllegalComponentException();
-        }
-
+        Constructor<Implementation> constructor = getInjectConstructor(implementation);
         providers.put(componentClass, (Provider<Type>) () -> {
             try {
-                Constructor<Implementation> constructor = getInjectConstructor(implementation);
                 // get dependency instance
                 Object[] dependencies = Arrays.stream(constructor.getParameters())
                     .map(p -> get(p.getType()))
                     .toArray();
                 return (Type) constructor.newInstance(dependencies);
-            } catch (Exception e) {
+            } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
 
-    private <T> Constructor<T> getInjectConstructor(Class<T> implementation){
-        Stream<Constructor<?>> injectConstructors = Arrays.stream(implementation.getConstructors())
-            .filter(c -> c.isAnnotationPresent(Inject.class));
-        return (Constructor<T>) injectConstructors
-            .findFirst()
-            .orElseGet(() -> getDefaultConstructor(implementation));
-    }
-
-    private <T> Constructor<T> getDefaultConstructor(Class<T> implementation) {
-        try {
-            return implementation.getConstructor();
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
+    private <T> Constructor<T> getInjectConstructor(Class<T> implementation) {
+        List<Constructor<?>> injectConstructors = Arrays.stream(implementation.getConstructors())
+            .filter(c -> c.isAnnotationPresent(Inject.class)).collect(Collectors.toList());
+        if (injectConstructors.size() > 1) {
+            throw new IllegalComponentException();
         }
+        return (Constructor<T>) injectConstructors.stream().findFirst().orElseGet(() -> {
+            try {
+                return implementation.getConstructor();
+            } catch (NoSuchMethodException e) {
+                throw new IllegalComponentException();
+            }
+        });
     }
 
     public <Type> Type get(Class<Type> type) {
